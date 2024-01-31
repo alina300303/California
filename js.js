f519a879e274cf662c7c22784da68a687bb7df7c
khvznursultan <nursultan 311204@icloud.com>
2024-01-31 22:12:20 +0600

    якорь и анимация поисковика

--- js.js
+++ js.js
@@ -1,4 +1,11 @@
 let registerbox = document.querySelector('.register')
+
+let search = document.querySelector('.input__search')
+
 let popup = () =>{
 registerbox.classList.toggle('active')
+}
+
+let search_popup = () =>{
+    search.classList.toggle('active')
 }

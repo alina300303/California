a6397dbb3422a1766e75ffd4284833331e094e31
BekjanCodify <dzaparovbekzan@gmail.com>
2024-02-01 23:30:49 +0600

    burger and js star

--- index.js
+++ index.js
@@ -4,7 +4,37 @@ const star3 = document.getElementById('3')
 const star4 = document.getElementById('4')
 const star5 = document.getElementById('5')
 
+const star6 = document.getElementById('6')
+const star7 = document.getElementById('7')
+const star8 = document.getElementById('8')
+const star9 = document.getElementById('9')
+const star10 = document.getElementById('10')
 
+const star11 = document.getElementById('11')
+const star12 = document.getElementById('12')
+const star13 = document.getElementById('13')
+const star14 = document.getElementById('14')
+const star15 = document.getElementById('15')
+
+const star16 = document.getElementById('16')
+const star17 = document.getElementById('17')
+const star18= document.getElementById('18')
+const star19= document.getElementById('19')
+const star20 = document.getElementById('20')
+
+const star21 = document.getElementById('21')
+const star22 = document.getElementById('22')
+const star23 = document.getElementById('23')
+const star24 = document.getElementById('24')
+const star25 = document.getElementById('25')
+
+const star26 = document.getElementById('26')
+const star27 = document.getElementById('27')
+const star28 = document.getElementById('28')
+const star29 = document.getElementById('29')
+const star30 = document.getElementById('30')
+
+// 1star
 function star_1(){
     star1.style.color = '#C72535'
     star2.style.color = '#DCE5E2'
@@ -40,4 +70,187 @@ function star_5(){
     star3.style.color = '#C72535'
     star4.style.color = '#C72535'
     star5.style.color = '#C72535'
+}
+
+// 2star
+function star_6(){
+    star6.style.color = '#C72535'
+    star7.style.color = '#DCE5E2'
+    star8.style.color = '#DCE5E2'
+    star9.style.color = '#DCE5E2'
+    star10.style.color = '#DCE5E2'
+}
+function star_7(){
+    star6.style.color = '#C72535'
+    star7.style.color = '#C72535'
+    star8.style.color = '#DCE5E2'
+    star9.style.color = '#DCE5E2'
+    star10.style.color = '#DCE5E2'
+}
+function star_8(){
+    star1.style.color = '#C72535'
+    star2.style.color = '#C72535'
+    star3.style.color = '#C72535'
+    star4.style.color = '#DCE5E2'
+    star5.style.color = '#DCE5E2'
+}
+function star_9(){
+    star1.style.color = '#C72535'
+    star2.style.color = '#C72535'
+    star3.style.color = '#C72535'
+    star4.style.color = '#C72535'
+    star5.style.color = '#DCE5E2'
+}
+function star_10(){
+    star6.style.color = '#C72535'
+    star7.style.color = '#C72535'
+    star8.style.color = '#C72535'
+    star9.style.color = '#C72535'
+    star10.style.color = '#C72535'
+}
+// 3star
+function star_11(){
+    star11.style.color = '#C72535'
+    star12.style.color = '#DCE5E2'
+    star13.style.color = '#DCE5E2'
+    star14.style.color = '#DCE5E2'
+    star15.style.color = '#DCE5E2'
+}
+function star_12(){
+    star11.style.color = '#C72535'
+    star12.style.color = '#C72535'
+    star13.style.color = '#DCE5E2'
+    star14.style.color = '#DCE5E2'
+    star15.style.color = '#DCE5E2'
+}
+function star_13(){
+    star11.style.color = '#C72535'
+    star12.style.color = '#C72535'
+    star13.style.color = '#C72535'
+    star14.style.color = '#DCE5E2'
+    star15.style.color = '#DCE5E2'
+}
+function star_14(){
+    star11.style.color = '#C72535'
+    star12.style.color = '#C72535'
+    star13.style.color = '#C72535'
+    star14.style.color = '#C72535'
+    star15.style.color = '#DCE5E2'
+}
+function star_15(){
+    star11.style.color = '#C72535'
+    star12.style.color = '#C72535'
+    star13.style.color = '#C72535'
+    star13.style.color = '#C72535'
+    star14.style.color = '#C72535'
+    star15.style.color = '#C72535'
+}
+// 4star
+function star_16(){
+    star16.style.color = '#C72535'
+    star17.style.color = '#DCE5E2'
+    star18.style.color = '#DCE5E2'
+    star19.style.color = '#DCE5E2'
+    star20.style.color = '#DCE5E2'
+}
+function star_17(){
+    star16.style.color = '#C72535'
+    star17.style.color = '#C72535'
+    star18.style.color = '#DCE5E2'
+    star19.style.color = '#DCE5E2'
+    star20.style.color = '#DCE5E2'
+}
+function star_18(){
+    star16.style.color = '#C72535'
+    star17.style.color = '#C72535'
+    star18.style.color = '#C72535'
+    star19.style.color = '#DCE5E2'
+    star20.style.color = '#DCE5E2'
+}
+function star_19(){
+    star16.style.color = '#C72535'
+    star17.style.color = '#C72535'
+    star18.style.color = '#C72535'
+    star19.style.color = '#C72535'
+    star20.style.color = '#DCE5E2'
+}
+function star_20(){
+    star16.style.color = '#C72535'
+    star17.style.color = '#C72535'
+    star18.style.color = '#C72535'
+    star19.style.color = '#C72535'
+    star20.style.color = '#C72535'
+}
+// 5star
+function star_21(){
+    star21.style.color = '#C72535'
+    star22.style.color = '#DCE5E2'
+    star23.style.color = '#DCE5E2'
+    star24.style.color = '#DCE5E2'
+    star25.style.color = '#DCE5E2'
+}
+function star_22(){
+    star21.style.color = '#C72535'
+    star22.style.color = '#C72535'
+    star23.style.color = '#DCE5E2'
+    star24.style.color = '#DCE5E2'
+    star25.style.color = '#DCE5E2'
+}
+function star_23(){
+    star21.style.color = '#C72535'
+    star22.style.color = '#C72535'
+    star23.style.color = '#C72535'
+    star24.style.color = '#DCE5E2'
+    star25.style.color = '#DCE5E2'
+}
+function star_24(){
+    star21.style.color = '#C72535'
+    star22.style.color = '#C72535'
+    star23.style.color = '#C72535'
+    star24.style.color = '#C72535'
+    star25.style.color = '#DCE5E2'
+}
+function star_25(){
+    star21.style.color = '#C72535'
+    star22.style.color = '#C72535'
+    star23.style.color = '#C72535'
+    star23.style.color = '#C72535'
+    star24.style.color = '#C72535'
+    star25.style.color = '#C72535'
+}
+// 6star
+function star_26(){
+    star26.style.color = '#C72535'
+    star27.style.color = '#DCE5E2'
+    star28.style.color = '#DCE5E2'
+    star29.style.color = '#DCE5E2'
+    star30.style.color = '#DCE5E2'
+}
+function star_27(){
+    star26.style.color = '#C72535'
+    star27.style.color = '#C72535'
+    star28.style.color = '#DCE5E2'
+    star29.style.color = '#DCE5E2'
+    star30.style.color = '#DCE5E2'
+}
+function star_28(){
+    star26.style.color = '#C72535'
+    star27.style.color = '#C72535'
+    star28.style.color = '#C72535'
+    star29.style.color = '#DCE5E2'
+    star30.style.color = '#DCE5E2'
+}
+function star_29(){
+    star26.style.color = '#C72535'
+    star27.style.color = '#C72535'
+    star28.style.color = '#C72535'
+    star29.style.color = '#C72535'
+    star30.style.color = '#DCE5E2'
+}
+function star_30(){
+    star26.style.color = '#C72535'
+    star27.style.color = '#C72535'
+    star28.style.color = '#C72535'
+    star29.style.color = '#C72535'
+    star30.style.color = '#C72535'
 }

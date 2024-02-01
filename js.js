b647f8d249280c2d5b8d5dab1781bd473c0492f1
BekjanCodify <dzaparovbekzan@gmail.com>
2024-02-01 18:04:21 +0600

    bekzhan

--- js.js
+++ js.js
@@ -1,4 +1,9 @@
 let registerbox = document.querySelector('.register')
+
 let popup = () =>{
 registerbox.classList.toggle('active')
+}
+
+let popout = () =>{
+    registerbox.classList.toggle('cancel')
 }
